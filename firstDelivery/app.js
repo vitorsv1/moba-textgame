7a22aa5e9aac554f2316b5e269a5d1ac6d2d68f2
carachancla <pauserra.org@gmail.com>
2020-05-14 20:39:29 +0200

    added express, images and stylesheets are served

---
+++ firstDelivery/app.js
@@ -0,0 +1,61 @@
+
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+
+const root = __dirname;
+
+var app = express();
+
+// Maybe change to app router or just app for express use
+// The CSS and IMGs are not working in the champ-select page
+
+const serverStatic = (response,file)=>{
+	const fileToServe = path.join(root,file);
+	const stream = fs.createReadStream(fileToServe);
+
+	console.log('serving...');
+	
+	stream.on('data',(chunk)=>{
+		console.log( 'read:' + chunk);
+		response.write(chunk);
+	});
+	stream.on('end',function(){
+		response.end();
+		console.log( 'done reading.');
+	});
+}
+
+//routing
+// ======================================
+
+//print info on requests
+app.use(function(req, res, next) {
+	console.log("request for "+ req.method + " " + req.originalUrl);
+	next();
+});
+
+//express public files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',  function(req, res, next) {
+	console.log( 'serving a index');
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/char-select',  function(req, res, next) {
+	console.log( 'serving a char-select');
+	res.sendFile(path.join(__dirname, 'char-select.html'));
+});
+
+app.get('/fight',  function(req, res, next) {
+	console.log( 'serving a fight');
+	res.sendFile(path.join(__dirname, 'fight.html'));
+});
+
+
+
+
+module.exports = app;
---
+++ firstDelivery/character.js
@@ -0,0 +1,29 @@
+class ability {
+	constructor(cd, move) {
+		this.cd = cd;
+		this.currentCd = 0;
+		this.move = move;
+	}
+
+	use(objective){}
+
+}
+
+class characther{
+	constructor(name, hp, attack, ability1, ability2){
+		this.name = name;
+		this.hp = hp;
+		this.attack = attack;
+		this.ability1 = ability1;
+		this.ability2 = ability2;
+		this.currentHp = hp;
+	}
+
+	recieve
+
+
+
+
+
+
+} 
