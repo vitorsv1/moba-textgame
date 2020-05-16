387063e6e4601de8ab64fdd1800561340576911c
vitorsv1 <vitorsv1@al.insper.edu.br>
2020-05-16 01:38:18 -0300

    Fight test bug fixed seems normal now

--- firstDelivery/public/scritps/characterModuleTest.js
+++ firstDelivery/public/scritps/characterModuleTest.js
@@ -6,7 +6,7 @@ console.log("--------begin test");
 
 mage = aaaa.createCharacter(aaaa.Mage, "veigar :)", 15, 2, 2,3,"test desc");
 fighter = new aaaa.createCharacter(aaaa.Fighter, "sett :)", 20,5, 2,3, "test descript");
-ranger = new aaaa.Ranger(aaaa.Ranger, "ashe :)", 10, 10, 2,3, "test descri");
+ranger = new aaaa.createCharacter(aaaa.Ranger, "ashe :)", 10, 10, 2,3, "test descri");
 game = new aaaa.Fight(mage,fighter);
 
 console.log(mage.describe());
