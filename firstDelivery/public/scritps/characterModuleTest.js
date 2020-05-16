3c62ce242b9e70758d50a1011d32962e1007031a
vitorsv1 <vitorsv1@al.insper.edu.br>
2020-05-16 03:05:57 -0300

    object fix

--- firstDelivery/public/scritps/characterModuleTest.js
+++ firstDelivery/public/scritps/characterModuleTest.js
@@ -4,7 +4,7 @@ let rlSync = require('readline-sync'); //npm install readline-sync to use this t
 console.log("--------begin test");
 
 
-mage = aaaa.createCharacter(aaaa.Mage, "veigar :)", 15, 2, 2,3,"test desc");
+mage = new aaaa.createCharacter(aaaa.Mage, "veigar :)", 15, 2, 2,3,"test desc");
 fighter = new aaaa.createCharacter(aaaa.Fighter, "sett :)", 20,5, 2,3, "test descript");
 ranger = new aaaa.createCharacter(aaaa.Ranger, "ashe :)", 10, 10, 2,3, "test descri");
 game = new aaaa.Fight(mage,fighter);
