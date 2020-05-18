bd4df87adcc20d1b1bd3e5077252f661c82fa595
vitorsv1 <vitorsv1@al.insper.edu.br>
2020-05-18 00:13:46 -0300

    Everything prettier and chat module made

---
+++ firstDelivery/public/scripts/characterModuleTest.js
@@ -0,0 +1,19 @@
+let aaaa = require('./character');
+
+let rlSync = require('readline-sync'); //npm install readline-sync to use this test
+console.log("--------begin test");
+
+
+mage = new aaaa.createCharacter(aaaa.Mage, "veigar :)", 15, 2, 2,3,"test desc");
+fighter = new aaaa.createCharacter(aaaa.Fighter, "sett :)", 20,5, 2,3, "test descript");
+ranger = new aaaa.createCharacter(aaaa.Ranger, "ashe :)", 10, 10, 2,3, "test descri");
+game = new aaaa.Fight(mage,fighter);
+
+console.log(mage.describe());
+
+while (true) {
+    let command = rlSync.question("next turn: ");
+    game.progress(command);
+    console.log(game.getFightStatus());
+}
+console.log("WTF");
