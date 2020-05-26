2d1cdd93201d9311a1e14bdd1230f9cd4b5ea083
vitorsv1 <vitorsv1@al.insper.edu.br>
2020-05-25 22:14:08 -0300

    Register validation

--- secondDelivery/public/scripts/register.js
+++ secondDelivery/public/scripts/register.js
@@ -10,15 +10,26 @@ $(document).ready(()=>{
 
         myEmail = $('#email').val();
         myPass = password;
-        // Perform some validation here ...
-        
+
         $.post("http://localhost:3000/register",
                 {email:myEmail,pass:myPass},
                 (data)=>{
-                    if(data===''){
-                        window.location.href="/";
-                    }
+                    if(data === ''){
+                        window.location.replace("http://localhost:3000");
+                    } 
         });
 
     });
 });
+
+function validateForm(){
+    var email = $("#email").val();
+    var pass = $("#password").val();
+    var pass2 = $("#password-conf").val();
+
+    if(email == null || email == "", pass == null || pass == "", pass2 == null || pass2 == ""){
+        alert("Please Fill All Fields!!");
+        return false;
+    }
+
+}
